2989f215f755c380e20c7d141a4c03388a25b26e
hasanov3598 <bahramov3598@gmail.com>
2023-11-10 18:25:02 +0600

    first commit

---
+++ dart_application/bin/dart_application.dart
@@ -0,0 +1,104 @@
+void main() {
+  //1
+  // for (int i = 10; i <= 20; i++) {
+  //   print(i * i);
+  // }
+  // int n = int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for (int i = 1; i <= n; i++) {
+  //   summ += i;
+  // }
+  //2
+  // print(summ);
+  // print("ВВедите сумму");
+  // double s = double.parse(stdin.readLineSync()!);
+  //3
+  //print("Введите срок");
+  // int n = int.parse(stdin.readLineSync()!);
+  // for (int i = 1; i <= n; i++) {
+  //   s = s + (s * 0.03);
+  //   print("$i месяц $s");
+  // }
+//4
+  // for (int i = 20; i <= 50; i++) {
+  //   if (i % 3 == 0 && i % 5 != 0) {
+  //     print(i);
+  //   }
+  // }
+  //5
+  // int summ = 0;
+  // for (int i = 1; i <= 50; i++) {
+  //   if (i % 5 == 0 || i % 7 == 0) {
+  //     summ += i;
+  //   }
+  // }
+  // print(summ);
+  //6
+  // for (int i = 10; i <= 99; i++) {
+  //   if (i % 4 == 0 && i % 6 != 0) {
+  //     print(i);
+  //   }
+  // }
+  //7
+  // int summ = 0;
+  // for (int i = 100; i <= 200; i++) {
+  //   if (i % 17 == 0) {
+  //     summ += i;
+  //   }
+  // }
+  // print(summ);
+  //8
+
+  // print("Введите число");
+  // int n = int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for (int i = 1; i <= n; i++) {
+  //   summ = summ + (i * i);
+  // }
+  //9
+  // print(summ);
+  // print("Введите начало");
+  // int a = int.parse(stdin.readLineSync()!);
+  // print("Введите второе число");
+  // int b = int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for (int i = a; i <= b; i++) {
+  //   summ += i;
+  // }
+  // print(summ);
+  //10
+  // print("Введите год");
+  // int year = int.parse(stdin.readLineSync()!);
+  // if (year % 4 ==0) {
+  // print("високосный год");
+  // }
+  //11
+  // print("Введите число 1");
+  // int a = int.parse(stdin.readLineSync()!);
+  // print("Введите число 1");
+  // int b = int.parse(stdin.readLineSync()!);
+  // int result = 1;
+  // for (int i = a; i > -1; i++) {
+  //   if (i % a == 0 && i % b == 0) {
+  //     print(i);
+  //     break;
+  //   }
+  // }
+  //12
+  // int a = 123;
+  // String b = a.toString();
+  // int summ = 0;
+  // for (int i = 0; i < b.length; i++) {
+  //   summ += int.parse(b[i]);
+  // }
+  // print(summ);
+  //13
+  // String a = "qwq";
+
+  // String b = a.split("").reversed.toList().join();
+  // if (a == b) {
+  //   print("Палендром");
+  // } else {
+  //   print("Не палендром");
+  // }
+}
